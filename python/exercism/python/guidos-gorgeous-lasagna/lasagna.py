7000bb4cfacf577fa56d01edf589867e19823b34
strat <strat@strats-Mac-mini.local>
2026-04-28 18:46:24 +0300

    Clean up Currency Exchange solution

--- python/exercism/python/guidos-gorgeous-lasagna/lasagna.py
+++ python/exercism/python/guidos-gorgeous-lasagna/lasagna.py
@@ -8,11 +8,9 @@ of a module and its functions and/or classes.
 """
 
 
-#TODO: define your EXPECTED_BAKE_TIME (required) and PREPARATION_TIME (optional) constants below.
 EXPECTED_BAKE_TIME = 40
 PREPARATION_TIME = 2
 
-#TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
 def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
@@ -21,11 +19,6 @@ def bake_time_remaining(elapsed_bake_time):
     """
     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
-#TODO: Define the 'preparation_time_in_minutes()' function below.
-# To avoid the use of magic numbers (see: https://en.wikipedia.org/wiki/Magic_number_(programming)), you should define a PREPARATION_TIME constant.
-# You can do that on the line below the 'EXPECTED_BAKE_TIME' constant.
-# This will make it easier to do calculations, and make changes to your code.
-
 def preparation_time_in_minutes(number_of_layers):
     """Calculate preparation time.
 
@@ -33,8 +26,6 @@ def preparation_time_in_minutes(number_of_layers):
     :return: int - total preparation time in minutes.
     """
     return number_of_layers * PREPARATION_TIME
-
-#TODO: define the 'elapsed_time_in_minutes()' function below.
 
 def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     """Calculate the elapsed cooking time.
@@ -49,5 +40,3 @@ def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     """
     return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
 
-# TODO: Remember to go back and add docstrings to all your functions
-#  (you can copy and then alter the one from bake_time_remaining.)
