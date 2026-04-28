c295f2f72ff7d72119e2ddc0eacce75208582bd1
strat <strat@strats-Mac-mini.local>
2026-04-28 18:58:10 +0300

    Complete cleanup curreny-exchange

--- python/exercism/python/currency-exchange/exchange.py
+++ python/exercism/python/currency-exchange/exchange.py
@@ -48,7 +48,7 @@ def get_number_of_bills(amount, denomination):
     :return: int - number of bills that can be obtained from the amount.
     """
 
-    return int((amount // denomination))
+    return int(amount // denomination)
 
 
 
