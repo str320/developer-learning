b4e13b53d545f6001eb768fe29c59c2fd1de1358
strat <strat@strats-Mac-mini.local>
2026-05-06 17:29:36 +0300

    Completed exercism Armstrong Numbers

--- python/exercism/python/armstrong-numbers/armstrong_numbers.py
+++ python/exercism/python/armstrong-numbers/armstrong_numbers.py
@@ -1,3 +1,9 @@
 def is_armstrong_number(number):
-    number = number^number = number
-    return number
+    digits = str(number)
+    power = len(digits)
+    armstrong = [int(digit) ** power for digit in digits]
+    total = sum(armstrong)
+    return total == number 
+
+print(is_armstrong_number(153))
+
