0969036e63a528e5d06e3fdbcdfa9c446dbd5401
strat <strat@strats-Mac-mini.local>
2026-05-06 20:09:25 +0300

    Completed exercism grains

--- python/exercism/python/grains/grains.py
+++ python/exercism/python/grains/grains.py
@@ -1,6 +1,17 @@
 def square(number):
-    pass
+    if number < 1 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+    
+    return 2 ** (number - 1)
 
 
 def total():
-    pass
+    total_grains = 0
+
+    for number in range(1, 65):
+        total_grains = total_grains + square(number)
+    return total_grains
+
+
+print(square(2))
+print(total())
