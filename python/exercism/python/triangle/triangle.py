db5d738c1c24cc3561aa425246c838b7ea47e129
strat <strat@strats-Mac-mini.local>
2026-05-12 19:29:33 +0300

    Completed exercism Triangle

--- python/exercism/python/triangle/triangle.py
+++ python/exercism/python/triangle/triangle.py
@@ -1,10 +1,24 @@
-def equilateral(sides):
-    pass
+def is_triangle(sides):
+    a, b, c = sides
+    is_sides_positive = a > 0 and b > 0 and c > 0
+    follows_triangle_rule = a + b >= c and b + c >= a and c + a >= b
+    return follows_triangle_rule and is_sides_positive
 
+
+def equilateral(sides):
+    a, b, c = sides
+    is_equilateral = a == b and b == c
+    return is_equilateral and is_triangle(sides)
 
 def isosceles(sides):
-    pass
+    a, b, c = sides
+    is_isosceles = a == b or b == c or c == a
+    return is_isosceles and is_triangle(sides)
 
 
 def scalene(sides):
-    pass
+    a, b, c = sides 
+    is_scalene = a != b and b != c and c != a
+    return is_scalene and is_triangle(sides)
+
+
