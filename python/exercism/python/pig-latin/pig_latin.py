15f8c64547c7861af6b49099698c1f9c65cb5a43
strat <strat@strats-Mac-mini.local>
2026-05-15 19:08:50 +0300

    Completed exercism pig latin

--- python/exercism/python/pig-latin/pig_latin.py
+++ python/exercism/python/pig-latin/pig_latin.py
@@ -1,5 +1,7 @@
+"""Functions for translating English text into Pig Latin."""
 def translate_word(word):
-    vowels = ["a", "e", "i", "o","u"]
+    """Translate one English word into Pig Latin."""
+    vowels = ["a", "e", "i", "o", "u"]
     special_starts = ["xr", "yt"]
 
     first_letter = word[0]
@@ -12,24 +14,25 @@ def translate_word(word):
         return f"{word}ay"
     
     for index, letter in enumerate(word):
-            if letter == "q" and word[index + 1] == "u":
-                split_point = index + 2
-                prefix = word[:split_point]
-                rest = word[split_point:]
-                return f"{rest}{prefix}ay"
+        if letter == "q" and word[index + 1] == "u":
+            split_point = index + 2
+            prefix = word[:split_point]
+            rest = word[split_point:]
+            return f"{rest}{prefix}ay"
             
-            elif letter == "y" and index > 0:
-                 prefix = word[:index]
-                 rest = word[index:]
-                 return f"{rest}{prefix}ay"
+        if letter == "y" and index > 0:
+            prefix = word[:index]
+            rest = word[index:]
+            return f"{rest}{prefix}ay"
     
-            if letter in vowels:
-                prefix = word[:index]
-                rest = word[index:]
-                return f"{rest}{prefix}ay"
-        
+        if letter in vowels:
+            prefix = word[:index]
+            rest = word[index:]
+            return f"{rest}{prefix}ay"
+    return word    
 
 def translate(text):
+    """Translate English text into Pig Latin."""
     words = text.split()
     result = [translate_word(word) for word in words]
     return " ".join(result)
